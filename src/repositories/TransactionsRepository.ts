59e8296ed0001844d450e418d4db1589e3f0772b
Hugo C. Durães <hugocalheira@gmail.com>
2021-03-09 18:40:52 -0300

    Passing tests

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+import { v4 as uuid } from 'uuid';
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
 }
 
 class TransactionsRepository {
@@ -14,15 +21,33 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let income = 0;
+    let outcome = 0;
+    this.transactions.forEach(transaction => {
+      income += transaction.type === 'income' ? transaction.value : 0;
+      outcome += transaction.type === 'outcome' ? transaction.value : 0;
+    });
+    const total = income - outcome;
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = {
+      id: uuid(),
+      title,
+      type,
+      value,
+    };
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import app from './app';
+import routes from './routes';
+
+app.use(routes);
 
 app.listen(3333, () => {
+  // eslint-disable-next-line no-console
   console.log('🚀 Server started on port 3333!');
 });
